d42058a8251967ecbc3702f6dd8f3af526c2d4b5
David L. Inman <davidlevere@gmail.com>
2020-03-24 13:33:14 -0400

    setting up initial parsing code; use statement trigger in main parser

---
+++ src/parse/ast.rs
@@ -0,0 +1,19 @@
+
+
+pub struct Module {
+    pub type_statements : Vec<Type>,
+    pub fun_statements : Vec<Fun>,
+    pub use_statements : Vec<Use>,
+}
+
+pub enum Type {
+
+}
+
+pub enum Fun {
+
+}
+
+pub enum Use {
+
+}
--- src/parse/top_level_parser.rs
+++ src/parse/top_level_parser.rs
@@ -1,5 +1,29 @@
 
+use super::ast::*;
 
-pub fn parse( input : &str ) {
+pub fn parse( input : &str ) -> Result<Module, String> {
+    fn p( mut input : &[char] ) -> Result<Module, String> {
+        let mut use_statements : Vec<Use> = vec![];
+        let mut fun_statements : Vec<Fun> = vec![];
+        let mut type_statements : Vec<Type> = vec![];
+        loop {
+            match input {
+                [] => break,
+                [x, rest @ .. ] if x.is_whitespace() => input = rest,
+                ['u', 's', 'e', x, rest @ .. ] if x.is_whitespace() => { 
+                    let (use_statement, new_input) = parse_use(rest)?; 
+                    use_statements.push( use_statement );
+                    input = new_input
+                },
+                _ => println!("else"),
+            }
+        }
+        Ok(Module { use_statements, fun_statements, type_statements })
+    }
 
+    p(&input.chars().collect::<Vec<char>>())
+}
+
+fn parse_use( input : &[char] ) -> Result<(Use, &[char]), String> {
+    Err("blah".to_string())
 }
