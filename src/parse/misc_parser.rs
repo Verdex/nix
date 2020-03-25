1b91927f1cd9026fedc910a0d6ccc793890fdb92
David L. Inman <davidlevere@gmail.com>
2020-03-25 16:55:41 -0400

    created expect misc_parser

--- src/parse/misc_parser.rs
+++ src/parse/misc_parser.rs
@@ -11,6 +11,17 @@ pub fn clear_whitespace( mut input : &[char] ) -> &[char] {
     }
 }
 
+pub fn expect<'a>( mut input : &'a [char], sequence : &str ) -> Result<&'a [char], String> {
+    for c in sequence.chars() {
+        match input {
+            [] => return Err( format!( "Encountered end of file when expecting {} of {}", c, sequence ) ),
+            [x, rest @ ..] if *x == c => input = rest,
+            [x, ..] => return Err( format!( "Encountered {} when expecting {} of {}", x, c, sequence ) ),
+        }
+    }
+    Ok( input )
+}
+
 pub fn parse_symbol( mut input : &[char] ) -> Result<(Symbol, &[char]), String> {
 
     let mut ret : Vec<char> = vec![];
@@ -51,21 +62,52 @@ mod Test {
     #[test]
     fn should_parse_symbol() {
         let input = "symbol ".chars().collect::<Vec<char>>();
-        let (sym, _) = parse_symbol(&input).unwrap();
+        let (sym, new_input) = parse_symbol(&input).unwrap();
         assert_eq!( sym.value, "symbol".to_string()  );
+        assert_eq!( new_input.iter().collect::<String>(), " " );
     }
 
     #[test]
     fn should_parse_symbol_with_numbers() {
         let input = "s123ymbol ".chars().collect::<Vec<char>>();
-        let (sym, _) = parse_symbol(&input).unwrap();
+        let (sym, new_input) = parse_symbol(&input).unwrap();
         assert_eq!( sym.value, "s123ymbol".to_string()  );
+        assert_eq!( new_input.iter().collect::<String>(), " " );
     }
 
     #[test]
     fn should_parse_symbol_with_underscore() {
         let input = "_sym_bol_ ".chars().collect::<Vec<char>>();
-        let (sym, _) = parse_symbol(&input).unwrap();
+        let (sym, new_input) = parse_symbol(&input).unwrap();
         assert_eq!( sym.value, "_sym_bol_".to_string()  );
+        assert_eq!( new_input.iter().collect::<String>(), " " );
+    }
+
+    #[test]
+    fn should_expect_single_char_string() {
+        let input = "&".chars().collect::<Vec<char>>();
+        let new_input = expect( &input, "&" ).unwrap();
+        assert_eq!( new_input.iter().collect::<String>(), "" );
+    }
+    
+    #[test]
+    fn should_expect_multi_char_string() {
+        let input = "&&".chars().collect::<Vec<char>>();
+        let new_input = expect( &input, "&&" ).unwrap();
+        assert_eq!( new_input.iter().collect::<String>(), "" );
+    }
+
+    #[test]
+    fn should_expect_zero_char_string() {
+        let input = "&&".chars().collect::<Vec<char>>();
+        let new_input = expect( &input, "" ).unwrap();
+        assert_eq!( new_input.iter().collect::<String>(), "&&" );
+    }
+
+    #[test]
+    fn should_fail_expect_on_mismatch() {
+        let input = "&&&".chars().collect::<Vec<char>>();
+        let result = expect( &input, "&*&" );
+        assert_eq!( matches!( result, Err(_) ), true );
     }
 }
